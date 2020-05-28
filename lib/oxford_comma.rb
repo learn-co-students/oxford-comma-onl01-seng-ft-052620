feb3f8ba32dc26ce13c788cfa5e841e004d41575
Nroulston <nicolas.roulston@gmail.com>
2020-05-28 13:11:56 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+require "pry"
+
 def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        array.last.prepend("and ")
+        array.join(", ")
+    end
+
+#     array.join(" and ")
+#   if array.length > 2
+#     array.last.prepend("and ")
+#   end
+  
 
 end
