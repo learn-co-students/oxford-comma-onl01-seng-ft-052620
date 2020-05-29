42c85ec4636e831bb9c0bb0f3603723b675e354e
Franceline Casanas <franceline90@gmail.com>
2020-05-29 02:39:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  # binding.pry
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  else
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  end
 end
+
+
+# returns a string without any additional fomatting when given a 1-element array (FAILED - 1)
+# adds 'and' between elements when given a 2-element array (FAILED - 2)
+# adds commas plus a final 'and' when given a 3-element array (FAILED - 3)
+# correctly formats arrays of lengths greater than three (FAILED - 4)
