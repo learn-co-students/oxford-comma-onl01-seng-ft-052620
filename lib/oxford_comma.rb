88a213c85b9bcfc562ff3e60619dfca0b32ae789
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-01 00:06:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array[0]
+  elsif array.length == 2
+    array[0] + " and " + array[1]
+  elsif array.length > 2
+    counter = 0
+    new_arr = array[counter] 
+    counter = counter + 1
+ while counter != array.length - 1
+      new_arr += ", " + array[counter]
+      counter = counter + 1      
+    end
+    new_arr += ", and " + array[counter]
+ end
 end
